e452cdf39f0b24d5bc0406947da46840c8332918
fakhrilak <fakhrilak23@gmail.com>
2020-10-14 01:22:14 +0700

    Updates

---
+++ precache-manifest.cb3a004ddb56abd00cbc5844fa8fdab4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eab9b94207f87a09798fb7003bef17d",
+    "url": "/frontend-iotku/index.html"
+  },
+  {
+    "revision": "77d8d35c73529c926d48",
+    "url": "/frontend-iotku/static/css/main.1b3a15ea.chunk.css"
+  },
+  {
+    "revision": "b863b85184d246e0ad93",
+    "url": "/frontend-iotku/static/js/2.fa665e2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/frontend-iotku/static/js/2.fa665e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77d8d35c73529c926d48",
+    "url": "/frontend-iotku/static/js/main.642bbe5c.chunk.js"
+  },
+  {
+    "revision": "844d50279774b853c87b",
+    "url": "/frontend-iotku/static/js/runtime-main.6b72a9f2.js"
+  },
+  {
+    "revision": "c1cc3d530d59c6ceaf55681cf4e3ce79",
+    "url": "/frontend-iotku/static/media/logo.c1cc3d53.png"
+  }
+]);
